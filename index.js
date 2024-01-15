04e02d4f33a54d01ebc0af5ef28403ef98d77687
nsleite <nivaldosleite@hotmail.com>
2024-01-15 11:05:44 -0300

    create click function

--- index.js
+++ index.js
@@ -1,35 +1,106 @@
 let colors = ['red', 'blue', 'green', 'yellow'];
-let order = [];
+let gameOrder = [];
 let clickOrder = [];
 let score = 0;
 var time = 1000;
 
-const red = document.querySelector(".red");
-const blue = document.querySelector(".blue");
-const green = document.querySelector(".green");
-const yellow = document.querySelector(".yellow");
+// const red = document.querySelector(".red");
+// const blue = document.querySelector(".blue");
+// const green = document.querySelector(".green");
+// const yellow = document.querySelector(".yellow");
 
 let createOrder = () => {
     let randomColor = Math.floor(Math.random() * 4);
-    order[order.length] = randomColor;
+    gameOrder[gameOrder.length] = randomColor;
 
-    order.forEach((element, index) =>{
+    gameOrder.forEach((element, index) =>{
         var color = document.querySelector(`.${colors[element]}`);
         lightColor(color, index + 1);
     });
 }
 
-let lightColor = (element, number) => {
-    number = number * time;
+let lightColor = (element, index) => {
+    console.log('Element:', element);
+    index = index * time;
     setTimeout( ()=> {
         element.classList.add("selected");
-    }, number - time/2);
+    }, index - time/2);
     
     setTimeout( ()=> {
         element.classList.remove("selected");
-    }, number);
+    }, index);
 }
 
-for(let i = 0; i<10; i++){
-    createOrder();
+let checkOrder = () => {
+    for(let i in clickOrder){
+        if (clickOrder[i] != gameOrder[i]){
+            gameOver();
+            break;
+        }
+        score ++;
+    }
+    if(clickOrder.length == gameOrder.length){
+        alert(`SCORE: ${score}`);
+        nextLevel();
+    }
 }
+
+// let click = (color) =>{
+//     clickOrder[clickOrder.length] = color;
+// }
+
+
+// for(let i = 0; i<10; i++){
+//     createOrder();
+// }
+
+let gameOver = () =>{
+    alert(`Game Over! \n Score: ${score}`);
+    gameOrder = [];
+    clickOrder = [];
+    startGame();
+}
+// function click(){
+//     document.querySelectorAll("div")
+//             .forEach((element)=>{
+//                 element.addEventListener("click", function(event){
+//                     colorClicked = event.srcElement.classList.value;
+//                     console.log(colorClicked, getNumber(colorClicked));
+//                 });
+//             })
+// }
+
+let clickArray = (colorClicked) => {
+    numberClicked = getNumber(colorClicked);
+    if(numberClicked === undefined){
+        return;
+    }
+    clickOrder.push(numberClicked);
+    divElement = document.querySelector(`.${colorClicked}`)
+    divElement.classList.add("selected");
+    setTimeout(()=>{
+        divElement.classList.remove("selected");
+    },150);
+    
+}
+
+
+function click(){
+    document.addEventListener("click", function(event){
+        colorClicked = event.srcElement.classList.value;
+        clickArray(colorClicked);
+    });
+}
+function getNumber(color){
+    var colorNumber = {
+        "red" : 0,
+        "blue" : 1,
+        "green" : 2,
+        "yellow" : 3,
+    };
+    return (colorNumber[color]);
+}
+
+var arrays = [];
+// createOrder();
+click();
